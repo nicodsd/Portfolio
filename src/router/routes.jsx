aa88aab9b15bceb73c75226bf9ab5d28ee29e0ff
Nicolas <nilonb7@gmail.com>
2023-07-18 20:41:59 -0300

    tailwind

---
+++ src/router/routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Index from '../pages/Index'
+
+const router = createBrowserRouter([
+    { path: '/', element: <Index /> }
+  ])
+
+export default router
