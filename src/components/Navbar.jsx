5122e00c0a29e923369f1b891f4fee66a63e99b8
Nicolas <nilonb7@gmail.com>
2023-08-04 13:25:47 -0300

    dev-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link as Anchor } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="relative h-20 w-full">
+    <nav className="relative h-20 w-full text-mono">
       <div className="w-full border-b border-[#000000] text-[#0600ff] flex justify-between absolute px-3 py-[7px] items-center">
         <div className="w-8 h-8 bg-[#000000] rounded-full"></div>
         <div className='flex w-[20rem] justify-between font-medium'>
@@ -16,7 +16,7 @@ function Navbar() {
             <p>Trabajos</p>
           </Anchor>
         </div>
-        <p className="text-xl">`portafolio`</p>
+        <p className="text-xl font-semibold">`portafolio`</p>
       </div>
     </nav>
   )
