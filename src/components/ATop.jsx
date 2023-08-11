e034a9c2dfac2836b29de838b6a641ebb16f80b1
Nicolas <nilonb7@gmail.com>
2023-08-11 18:36:43 -0300

    div-1

--- src/components/ATop.jsx
+++ src/components/ATop.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import arr from '../../public/Images/diseño-grafico/300ppi/FlechaUp.svg';
 
 function ATop() {
-    
+
     const [hover, setHover] = useState(false);
-    function parriba(){
+
+    function parriba() {
         window.scroll({
             top: 100,
             left: 100,
@@ -20,10 +21,10 @@ function ATop() {
             onMouseLeave={() => {
                 setHover(!hover);
             }}
-            className='flex fixed z-50 justify-center items-center right-24 bottom-32'
+            className='flex fixed z-50 justify-center items-center right-20 bottom-20'
         >
             {hover ? (
-                <div onClick={()=> {
+                <div onClick={() => {
                     parriba()
                 }}>
                     <div className='bg-[#FFD400] absolute rounded-[1rem] w-[7vh] h-[5.5vh] flex items-center justify-center'>
@@ -32,8 +33,8 @@ function ATop() {
                 </div>
             ) : (
                 <div>
-                    <div className='bg-[#FFD400] absolute rounded-[1rem] w-[7vh] h-[5.5vh] flex items-center justify-center'>
-                        <div className='bg-[#000000] absolute rounded-[1rem] w-[5vh] h-[1.5vh]'></div>
+                    <div className='bg-[#ffd500] absolute rounded-[1rem] w-[7vh] h-[5.5vh] flex items-center justify-center'>
+                        <div className='bg-[#0600ff] absolute rounded-[1rem] w-[5vh] h-[1.5vh]'></div>
                     </div>
                 </div>
             )}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link as Anchor } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="relative h-20 w-full text-mono">
-      <div className="w-full bg-[#0600ff] text-[#ffffff] flex justify-between absolute px-3 py-[7px] items-center">
+    <nav className="relative w-full text-mono">
+      <div className="w-full bg-[#ffffff] text-[#0600ff] flex justify-between absolute px-3 py-[7px] items-center">
         <div className='flex w-[23rem] justify-between font-medium items-center'>
-        <div className="w-8 h-8 bg-[#ffffff] rounded-full"></div>
+        <div className="w-8 h-8 bg-[#0600ff] rounded-full"></div>
           <Anchor>
             <p>Inicio</p>
           </Anchor>
