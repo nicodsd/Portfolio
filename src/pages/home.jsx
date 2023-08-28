98d9408c4580bf6c870695c18b1247eb56a5bcfd
Nicolas <nilonb7@gmail.com>
2023-08-28 16:25:59 -0300

    div-1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -26,11 +26,11 @@ function Index() {
       <div className='flex flex-col justify-between min-h-screen h-fit bg-[#e2e8ee]'>
         <ATopce />
         <div className='h-fit cuerpo text-mono mt-24'>
-          <p className='absolute text-[20rem] text-[#FFD400] text-mono font-semibold top-14 right-[50%] '>*</p>
+          <p className='absolute text-[20rem] text-[#FFD400] text-mono font-semibold top-14 right-[50%] selection:bg-transparent'>*</p>
           <div className='flex relative items-end my-10 justify-between'>
             <div className='flex flex-col font-semibold'>
-              <p className='nicoBarrera nico text-[11vw] h-[8.5vw]'> Nico </p>
-              <p className='nicoBarrera text-[11vw] h-[14vw]'> Barrera </p>
+              <p className='nicoBarrera nico text-[11vw] h-[8.5vw] selection:bg-transparent'> Nico </p>
+              <p className='nicoBarrera text-[11vw] h-[14vw] selection:bg-transparent'> Barrera </p>
             </div>
             <svg className='z-10 h-[22vw]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 743.92 350.37"><defs></defs><g id="Capa_2" data-name="Capa 2"><g id="por_encima" data-name="por encima"><path className="cls-1" d="M532.27,283.55a68.76,68.76,0,0,1-25.48,11.39c-40.64,7.53-72.59-3.7-90-49.32-18.47-48.33-2.23-85,40.81-89.2,17.71-1.71,33.9.91,47.93,8.27a143,143,0,0,1,15-46.43c-24-12.19-51.63-16.52-81.71-13.58-74.6,7.31-106.43,85.14-74.39,169,30.21,79.1,89.23,84.26,159.7,71.2,13.1-2.44,37-11.35,55.41-29Q550.85,305.88,532.27,283.55Z" /><path className="cls-1" d="M720.15,107q-26.76-33.27-78.68-36.48Q592.1,67.47,560,98.36t-35.37,83q-3.33,53.84,23.48,88.06t78.73,37.45q50.85,3.15,82.1-27.16T743.54,195Q746.93,140.28,720.15,107Zm-31.93,84.52q-2.24,36.16-18,54.84T629.51,263.5q-54.25-3.36-49.58-78.68,2.18-35.12,18.07-53.77t40.78-17.12Q692.83,117.28,688.22,191.53Z" /><path className="cls-1" d="M249.54,73.86l18.22,118.63L116.28,0H46.82l94.75,125.69c-61.8,16.47-96,47.32-120.06,95.94C0,265,0,318.88,0,318.88H69.45C69.64,244.69,115.93,185.47,179.5,176l3.9,5.17,103.78,137.7h63.06l-37.64-245Z" /><circle className="cls-1" cx="349.52" cy="36.99" r="36.86" /></g></g></svg>
           </div>
@@ -83,8 +83,8 @@ function Index() {
           
           <div className='w-[55%] h-fit texto2 flex flex-col justify-center px-14 z-10'>
             <h2 className='text-2xl font-bold text-mono w-full'>Sobre mi<span className=' animate-pulse-slow'>_</span></h2>
-            <h2 className='text-7xl text-mono font-semibold text-[#FFD400] w-24 h-[65px] mt-6'>Primero,</h2>
-            <h2 className='text-7xl text-mono font-semibold text-[#FFD400] w-24'> hola</h2>
+            <h2 className='text-8xl text-mono font-semibold text-[#FFD400] w-24 h-[85px] mt-6'>Primero,</h2>
+            <h2 className='text-8xl text-mono font-semibold text-[#FFD400] w-24'> hola</h2>
             <h3 className='haches text-mono mt-8'>Introducción</h3>
             <p className='paraf-ini'>Me llamo Nicolas Barrera, soy una persona multifacética e inquieta.
               De pequeño me llamaba la atención las computadoras y las artes plásticas, tomando un gusto por los graffitis
