37499f491fe8ecaf1faf9c89e29c68ad2fd27f04
Nicolas <nilonb7@gmail.com>
2023-10-23 19:41:23 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Layout from './layouts/Layout';
-import Index from './pages/Home'; 
+import Index from './pages/home.jsx'; 
 import Detalles from './pages/Detalles/DetallesAmazing'; 
 import ErrorPage from './pages/ErrorPage'; 
 
