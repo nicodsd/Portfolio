bb4447512eedf10bbb4f84136cf88ab0035c7881
Nicolas <nilonb7@gmail.com>
2023-10-23 23:26:14 -0300

    fixs

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-let apiUrl = "https://portafolio-back-36us.onrender.com/"
+let apiUrl = "http://localhost:8000/"
 
 if (process.env.NODE_ENV==='production'){
-    apiUrl = process.env.REACT_APP_URL
+    apiUrl = process.env.REACT_APP_API
 }
 
 export default apiUrl
