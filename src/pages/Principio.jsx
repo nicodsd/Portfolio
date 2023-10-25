82628f9b0fedff62e5595bdd99c622cfe4dd8e2e
Nicolas <nilonb7@gmail.com>
2023-10-25 19:01:42 -0300

    cambios y mejoras

--- src/pages/Principio.jsx
+++ src/pages/Principio.jsx
@@ -8,19 +8,19 @@ import asterixco from '../../public/Images/diseño-grafico/300ppi/asterixco.svg'
 function Principio() {
     return (
         <>
-            <div className='h-screen mt-24 md:h-fit px-5 md:cuerpo text-mono md:mt-10 z-20 overflow-x-hidden'>
-                <img className='animate__fadeIn animate__animated absolute h-12 md:h-[11rem] text-[#FFD400] md:top-38 left-12 md:left-[41%] drop-shadow-xl z-20 asterixco' src={asterixco} alt="asterixco" />
+            <div className='mt-24 md:h-fit px-5 md:cuerpo text-mono md:mt-10 z-20 overflow-x-hidden'>
+                        <img className='animate__fadeIn animate__animated absolute h-[16vw] text-[#FFD400] md:top-38 left-[10%] sm:left-[10%] md:left-[37%] drop-shadow-xl z-20 asterixco' src={asterixco} alt="asterixco" />
                 <div className='flex flex-col items-center md:flex-row md:items-end justify-between'>
                     <div className='flex text-2xl md:-indent-2 flex-col font-[700] animate__fadeInLeft animate__animated'>
-                        <p className='nicoBarrera nico md:text-[10vw] h-5 md:h-[7.6vw]'> Nico </p>
-                        <p className='nicoBarrera md:text-[10vw] md:h-[5vw]'> Barrera </p>
+                        <p className='nicoBarrera nico text-[10vw] h-[8vw] md:h-[7.6vw]'> Nico </p>
+                        <p className='nicoBarrera text-[10vw] h-[10vw] md:h-[5vw]'> Barrera </p>
                     </div>
                     <svg className='z-10 w-[60vw] md:w-[46vw] animate__animated animate__backInRight' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 743.92 350.37"><defs></defs><g id="Capa_2" data-name="Capa 2"><g id="por_encima" data-name="por encima"><path className="cls-1" d="M532.27,283.55a68.76,68.76,0,0,1-25.48,11.39c-40.64,7.53-72.59-3.7-90-49.32-18.47-48.33-2.23-85,40.81-89.2,17.71-1.71,33.9.91,47.93,8.27a143,143,0,0,1,15-46.43c-24-12.19-51.63-16.52-81.71-13.58-74.6,7.31-106.43,85.14-74.39,169,30.21,79.1,89.23,84.26,159.7,71.2,13.1-2.44,37-11.35,55.41-29Q550.85,305.88,532.27,283.55Z" /><path className="cls-1" d="M720.15,107q-26.76-33.27-78.68-36.48Q592.1,67.47,560,98.36t-35.37,83q-3.33,53.84,23.48,88.06t78.73,37.45q50.85,3.15,82.1-27.16T743.54,195Q746.93,140.28,720.15,107Zm-31.93,84.52q-2.24,36.16-18,54.84T629.51,263.5q-54.25-3.36-49.58-78.68,2.18-35.12,18.07-53.77t40.78-17.12Q692.83,117.28,688.22,191.53Z" /><path className="cls-1" d="M249.54,73.86l18.22,118.63L116.28,0H46.82l94.75,125.69c-61.8,16.47-96,47.32-120.06,95.94C0,265,0,318.88,0,318.88H69.45C69.64,244.69,115.93,185.47,179.5,176l3.9,5.17,103.78,137.7h63.06l-37.64-245Z" /><circle className="cls-1" cx="349.52" cy="36.99" r="36.86" /></g></g></svg>
                 </div>
 
                 <div className='flex items-start justify-between w-full text-mono'>
                     <div className='flex mt-8 md:mt-10 w-full md:w-[50%] animate__fadeInLeft animate__animated'>
-                        <div className='linea1 h-1 w-[20%] '></div> 
+                        <div className='linea1 h-1 w-[20%] '></div>
                         <div className='linea2 h-1 w-[20%] '></div>
                         <div className='linea3 h-1 w-[20%] '></div>
                         <div className='linea bg-black h-1 w-[100%]'></div>
--- src/pages/Tecnologias.jsx
+++ src/pages/Tecnologias.jsx
@@ -10,12 +10,12 @@ function Tecnologias() {
     return (
         <div className='h-fit relative w-full text-mono flex flex-col px-5 lg:px-14 items-center justify-evenly'>
             <h2 id='tecnologias' className='w-full pt-20 text-2xl lg:text-3xl font-bold'>Tecnologias<span className=' animate-pulse-slow'>_</span></h2>
-            <img className='absolute h-20 z-30 lg:h-[7rem] bottom-24 lg:bottom-1/2 right-10 lg:right-44 animate-spin-slow' src={rueda} alt="rueda" />
+            <img className='absolute h-[4.5rem] z-30 lg:h-[7rem] bottom-[40vw] sm:bottom-[34vw] lg:bottom-[30vw] right-10 lg:right-[10vw] animate-spin-slow' src={rueda} alt="rueda" />
 
             <div className='w-full'>
                 <div className='flex w-full flex-col items-center gap-y-10 md:gap-4 z-10'>
 
-                    <div className='lg:h-[30vw] gap-y-10 md:gap-y-0 flex flex-col md:flex-row w-full md:justify-between items-center'>
+                    <div className='md:h-[45vw] lg:h-[30vw] gap-y-10 md:gap-y-0 flex flex-col md:flex-row w-full md:justify-between items-center'>
 
                         <div className='md:w-[50%] pb-6 border-b-2 md:border-b-0 border-[#0600ff] lg:border-e-4 lg:border-[#0600ff]'>
                             <p className='text-xs md:text-base font-[300] md:w-[80%] pt-2'>Mi trabajo comienza desde un maquetado en Figma buscando dar sentido a las ideas creativas, luego voy desarrollando las funcionalidades de las aplicaciones.</p>
@@ -23,11 +23,11 @@ function Tecnologias() {
 
                         <div className='flex z-10 md:w-[48%] justify-center'>
                             <div>
-                                <div className='bg-black md:p-2 p-1 text-[11px] md:text-sm rounded-ee-[15px] text-white'>
+                                <div className='bg-black md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-ee-[15px] text-white'>
                                     <p>Frontend</p>
                                     <p className='text-xs'>librerias & CMS</p>
                                 </div>
-                                <div className='bg-[#FF0080] md:p-2 relative z-20 p-1 text-[11px] md:text-sm rounded-se-[15px] text-white'>
+                                <div className='bg-[#FF0080] md:p-2 relative z-20 p-[7.5px] text-[11px] md:text-sm rounded-se-[15px] text-white'>
                                     <p>Html</p>
                                     <p>Javascript</p>
                                     <p>Css</p>
@@ -40,8 +40,8 @@ function Tecnologias() {
                             </div>
 
                             <div className='text-white flex flex-col mt-4'>
-                                <div className='bg-black md:p-2 p-1 text-[11px] md:text-sm rounded-ee-[15px] rounded-es-[15px]'>Backend</div>
-                                <div className='bg-[#FF0080] md:p-2 p-1 text-[11px] md:text-sm rounded-se-[15px] rounded-ss-[15px]'>
+                                <div className='bg-black md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-ee-[15px] rounded-es-[15px]'>Backend</div>
+                                <div className='bg-[#FF0080] md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-se-[15px] rounded-ss-[15px]'>
                                     <p>NodeJs</p>
                                     <p>ExpressJs</p>
                                     <p>php</p>
@@ -50,28 +50,31 @@ function Tecnologias() {
                             </div>
 
                             <div className='flex flex-col mt-4'>
-                                <div className='bg-[#000000] md:p-2 p-1 text-[11px] md:text-sm rounded-es-[15px] rounded-ee-[15px] text-white'>Base de Datos</div>
-                                <div className='bg-[#FF0080] md:p-2 p-1 text-[11px] md:text-sm rounded-ss-[15px] rounded-se-[15px] text-white'>
+                                <div className='bg-[#000000] md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-es-[15px] rounded-ee-[15px] text-white'>Base de Datos</div>
+                                <div className='bg-[#FF0080] md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-ss-[15px] rounded-se-[15px] text-white'>
                                     <p>Firebase</p>
                                     <p>MongoDB</p>
+                                    <p>MySql</p>
                                 </div>
                             </div>
                             <div className='flex flex-col mt-4'>
-                                <div className='bg-[#000000] md:p-2 p-1 text-[11px] md:text-sm rounded-es-[15px] text-white'>Herramientas</div>
-                                <div className='bg-[#FFD400] md:p-2 p-1 text-[11px] md:text-sm rounded-ss-[15px]'>
+                                <div className='bg-[#000000] md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-es-[15px] text-white'>Herramientas</div>
+                                <div className='bg-[#FFD400] md:p-2 p-[7.5px] text-[11px] md:text-sm rounded-ss-[15px]'>
                                     <p>Postman</p>
                                     <p>Figma</p>
+                                    <p>Git</p>
+                                    <p>GitHub</p>
                                 </div>
                             </div>
-                            <img className='absolute h-[25vh] md:h-[20vw] z-10 top-64 md:rotate-12' src={flecha} alt="flecha" />
+                            <img className='absolute h-[60vw] sm:h-[30vw] z-10 bottom-[24vw] md:rotate-12' src={flecha} alt="flecha" />
                         </div>
 
                     </div>
-                    <div className='relative w-full h-[28vw] flex items-center selection:bg-transparent'>
-                        <img className='absolute h-fit w-[45vw] md:w-[44vw] rounded-md right-1/4 z-10 shadow-[0px_12px_0_rgba(0,0,250,1)]' src={img} alt="imagen" />
-                        <img className='absolute h-fit w-[36vw] opacity-95 md:w-[30vw] rounded-md right-0 z-0 shadow-[0px_9px_0_rgba(260,0,100,1)]' src={img2} alt="imagen_2" />
-                        <img className='absolute h-fit w-[36vw] opacity-95 md:w-[30vw] rounded-md left-0 z-0 shadow-[0px_9px_0_rgba(260,0,100,1)]' src={img3} alt="imagen_3" />
-                        <img className='z-20 absolute bottom-0' src={devsa} alt="devsarrollo" />
+                    <div className='relative w-full h-[45vw] md:h-[30vw] flex items-center justify-center selection:bg-transparent z-10'>
+                        <img className='absolute h-fit w-[50vw] md:w-[44vw] rounded md:rounded-md shadow-[0px_5px_0_rgba(0,0,250,1)] z-10 md:shadow-[0px_12px_0_rgba(0,0,250,1)]' src={img} alt="imagen" />
+                        <img className='absolute h-fit w-[40vw] rotate-6 md:rotate-0 opacity-95 md:w-[30vw] rounded md:rounded-md -right-2 shadow-[0px_4px_0_rgba(260,0,100,1)] md:shadow-[0px_9px_0_rgba(260,0,100,1)]' src={img2} alt="imagen_2" />
+                        <img className='absolute h-fit w-[40vw] -rotate-6 md:rotate-0 opacity-95 md:w-[30vw] rounded md:rounded-md -left-2 shadow-[0px_4px_0_rgba(260,0,100,1)] md:shadow-[0px_9px_0_rgba(260,0,100,1)]' src={img3} alt="imagen_3" />
+                        <img className='z-20 absolute bottom-0 opacity-0 md:opacity-100' src={devsa} alt="devsarrollo" />
                     </div>
                 </div>
             </div>
