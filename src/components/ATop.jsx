d730e8dcc0b706e80be0e3b33a5f51c5a4519103
Nicolas <nilonb7@gmail.com>
2023-10-25 01:46:24 -0300

    animaciones

--- src/components/ATop.jsx
+++ src/components/ATop.jsx
@@ -28,13 +28,13 @@ function ATop() {
                     parriba()
                 }}>
                     <div className='bg-[#ffd500] drop-shadow-md selection:bg-transparent cursor-pointer absolute rounded-[1rem] w-[55px] h-[45px] flex items-center justify-center'>
-                        <img className='z-10 w-5 absolute' src={arr} alt='' />
+                        <img className='z-10 w-5 absolute animate__animated animate__fadeInUp' src={arr} alt='' />
                     </div>
                 </div>
             ) : (
                 <div>
                     <div className='absolute rounded-[1rem] w-[7vh] h-[5vh] flex items-center justify-center'>
-                        <div className='border-white border-2 bg-[#000000] absolute rounded-[1rem] w-[45px] h-[12px]'></div>
+                        <div className='border-white border-2 animate__animated animate__fadeInUpBig bg-[#000000] absolute rounded-[1rem] w-[45px] h-[12px]'></div>
                     </div>
                 </div>
             )}
