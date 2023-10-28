3779d1233314b8e191c0b052fc5f6b618d503ceb
Nicolas <nilonb7@gmail.com>
2023-10-28 01:06:32 -0300

    descarga de curriculum

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import file from '../../public/Images/diseño-grafico/file_save.svg'
+import icon from '../../public/Images/diseño-grafico/file_save.svg'
+import pdf from '../../public/Images/contenido/PDF/Curriculum_Vitae_Barrera_Julio_Nicolas.pdf'
 
 function Curriculum() {
     return (
-        <div 
-        className='flex fixed z-40 justify-center items-center right-[54px] bottom-[115px]'
-        onClick={() => {
-            
-        }}>
+        <a
+            href={pdf}
+            download="Curriculum Vitae - Barrera Julio Nicolas"
+            className='flex fixed z-40 justify-center items-center right-[54px] bottom-[115px]'>
             <div className='bg-[#ffd500] drop-shadow-md selection:bg-transparent cursor-pointer absolute rounded-[1rem] w-[55px] h-[45px] flex items-center justify-center'>
-                <img className='z-10 w-7 absolute animate__animated animate__fadeInUp' src={file} alt='' />
+                <img className='z-10 w-7 absolute animate__animated animate__fadeInUp' src={icon} alt='' />
             </div>
-        </div>
+        </a>
     )
 }
 
