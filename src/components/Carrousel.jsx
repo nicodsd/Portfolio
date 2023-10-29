b9601b4fce2647a22b1a064c25c8a3888d18a9ee
Nicolas <nilonb7@gmail.com>
2023-10-29 18:27:18 -0300

    arreglos

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -6,26 +6,26 @@ import 'slick-carousel/slick/slick-theme.css';
 function WordCarousel() {
     const settings = {
         centerMode: true,
-        centerPadding:"100px",
+        centerPadding: "100px",
         speed: 200,
         infinite: true,
         cssEase: "ease",
         pauseOnHover: false,
         pauseOnFocus: false,
-        slidesToShow: 1, 
+        slidesToShow: 1,
         autoplay: true,
-        autoplaySpeed: 1500, 
+        autoplaySpeed: 1500,
         arrows: false
     };
 
-    const palabras = ["Diseño Gráfico", "Javascript", "Desarrollo Web & Apps", "Nico Barrera", "UX/UI", "Full Stack MERN", "Wordpress"];
+    const palabras = ["Diseño Gráfico", "Javascript", "Webs & Apps", "Nico Barrera", "UX/UI", "Full Stack MERN", "Wordpress"];
 
     return (
         <div className="palabra-carousel w-full">
             <Slider {...settings}>
                 {palabras.map((palabra, index) => (
-                    <div key={index} className="palabra-item border-[1px] hover:opacity-70 duration-150 p-1 md:p-2  border-[#000000] text-[#000000] bg-[#FFD400] text-center">
-                        <p className='palabras_slide text-[8px] md:text-lg text-mono'>{palabra}</p>
+                    <div key={index} className="palabra-item border-[1px] hover:opacity-70 duration-150 p-2 md:p-2 border-[#000000] text-[#000000] bg-[#FFD400] text-center">
+                        <p className='palabras_slide text-[11px] md:text-lg text-mono'>{palabra}</p>
                     </div>
                 ))}
             </Slider>
