5037dbc0b554ddc8fa7c4007bf6b2b1dfd579bed
Nicolas <nilonb7@gmail.com>
2023-10-31 06:11:42 -0300

    cambios peque

--- src/components/Detalles/CentroInfo.jsx
+++ src/components/Detalles/CentroInfo.jsx
@@ -13,7 +13,7 @@ function CentroInfo(props) {
         <>
             <div className='relative md:w-fit min-h-screen gap-y-16 flex flex-col pt-12 md:pt-3 md:px-5 lg:px-8 xl:px-10 p-4 z-20 md:pb-10'>
                 <div>
-                    <h1 style={{ color: color }} className="text-[3.5rem] leading-none md:text-[3rem] lg:text-[7vw] xl:text-[5.6rem] md:font-[300] lg:font-[100] mb-2 md:mb-2">{titulo}*</h1>
+                    <h1 style={{ color: color }} className="text-[3.7rem] leading-none md:text-[3rem] lg:text-[7vw] xl:text-[5.6rem] md:font-[300] lg:font-[100] mb-2 md:mb-2">{titulo}*</h1>
                     <p className="font-[300] leading-5 md:font-[400] text-sm">{descripcion}</p>
                 </div>
                 <div className='flex flex-col items-start gap-y-10 md:gap-y-8 md:font-[600]'>
