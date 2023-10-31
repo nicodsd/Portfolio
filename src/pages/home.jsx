1797a6729dbfc96bbea716638a1f5b1450caa8a6
Nicolas <nilonb7@gmail.com>
2023-10-31 01:05:30 -0300

    negro 2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,7 +16,7 @@ function Index() {
   return (
     <>
       <div className='flex text-mono flex-col h-fit scroll-smooth'>
-        <div className='fixed w-full h-[10.5rem] bottom-0 z-20 md:invisible bg-gradient-to-t from-[#000000] via-[#00000054] to-transparent opacity-80'></div>
+        <div className='fixed w-full h-[10.5rem] bottom-0 z-20 md:invisible bg-gradient-to-t from-[#000000c4] via-[#00000041] to-transparent opacity-80'></div>
         <Principio />
         <Presentacion />
         <Presentacion_2 />
