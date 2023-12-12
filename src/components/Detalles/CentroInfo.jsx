43eba884ab5dd5f1d21dd113bcc111c340d788ba
Nicolas <nilonb7@gmail.com>
2023-12-12 06:42:13 -0300

    cambios gen

--- src/components/Detalles/CentroInfo.jsx
+++ src/components/Detalles/CentroInfo.jsx
@@ -17,9 +17,9 @@ function CentroInfo(props) {
     return (
         <>
             <div style={{ backgroundColor: color }} className='w-full h-[20rem] fondo-blur animate__flash animate__animated animate__infinite blur-[200px] absolute opacity-30'></div>
-            <div className='w-screen bg-white md:max-w-[60vw] min-h-screen md:h-screen md:overflow-auto gap-y-16 flex flex-col pt-12 md:pt-3 md:px-5 lg:px-8 xl:px-10 px-4 pb-10 z-20 md:pb-[10vh] rounded-lg'>
+            <div className='w-screen bg-white md:max-w-[60vw] min-h-screen md:h-screen md:overflow-auto gap-y-16 flex flex-col pt-12 md:pt-6 md:px-5 lg:px-8 xl:px-10 px-4 pb-10 z-20 md:pb-[10vh] rounded-lg'>
                 <div>
-                    <h1 style={{ color: color }} className="text-[3.1rem] leading-none md:-indent-2 -tracking-[5px] md:text-[3rem] lg:text-[7vw] xl:text-[5.6rem] font-[400] md:font-[300] lg:font-[100] mb-2 md:mb-4">{titulo}*</h1>
+                    <h1 style={{ color: color }} className="text-[3.1rem] leading-none -tracking-[5px] md:text-[3rem] lg:text-[7vw] xl:text-[6rem] font-[400] md:font-[300] lg:font-[100] mb-2 md:mb-4">{titulo}*</h1>
                     <p className="font-[300] leading-5 text-sm text-[#3f3f3f]">{descripcion}</p>
                 </div>
                 <div className='flex flex-col items-start gap-y-10 md:font-[600]'>
